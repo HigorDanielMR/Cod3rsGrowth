682a344fba8840769fe0ddb0e1b6739bf4d031bb
Higor <higordaniel199@gmail.com>
2024-05-14 14:19:50 -0300

    Criando as classes e enum

---
+++ Cod3rsGrowth.Dominio/Entities/Carro.cs
@@ -0,0 +1,13 @@
+﻿using Cod3rsGrowth.Dominio.Enums;
+
+namespace Cod3rsGrowth.Dominio.Entities
+{
+    class Carro
+    {
+        public string Modelo { get; set; }
+        public int ID { get; set; }
+        public decimal ValorDoVeiculo { get; set; }
+        public bool Disponivel { get; set; }
+        public Cores Cor { get; set; }
+    }
+}
---
+++ Cod3rsGrowth.Dominio/Entities/Venda.cs
@@ -0,0 +1,14 @@
+﻿namespace Cod3rsGrowth.Dominio.Entities
+{
+    class Venda
+    {
+        public string Nome { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+        public string Telefone { get; set; }
+        public string CPF { get; set; }
+        public int Id { get; set; }
+        public int Total { get; set; }
+        public List<Carro> VendaVeiculo { get; set; }
+        public DateTime DataDeCompra { get; set; }
+    }
+}
