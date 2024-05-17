f0849c6b3c63c6d0b94fc6ac856a08eda5dc6c48
Higor <higordaniel199@gmail.com>
2024-05-17 11:22:35 -0300

    Criar TabelaSingleton, criar IServicoCarro, IServicoVenda, ServicoCarro, ServicoVenda e mudar algumas classes de lugar

---
+++ Cod3rsGrowth.Dominio/Servicos/ServicoCarro.cs
@@ -0,0 +1,8 @@
+﻿using Cod3rsGrowth.Dominio.Interfaces;
+
+namespace Cod3rsGrowth.Dominio.Servicos
+{
+    public class ServicoCarro : IServicoCarro
+    {
+    }
+}
--- Cod3rsGrowth.Infra/Interfaces/IRepositorio.cs
+++ Cod3rsGrowth.Infra/Interfaces/IRepositorio.cs
@@ -1,6 +1,6 @@
 ﻿using Cod3rsGrowth.Dominio.Entities;
 
-namespace Cod3rsGrowth.Dominio.Interfaces
+namespace Cod3rsGrowth.Infra.Interfaces
 {
     public interface IRepositorio
     {
