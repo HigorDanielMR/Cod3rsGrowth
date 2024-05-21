8f48349ad947c360b3154abdc3ca15d8dcfe9736
Higor <higordaniel199@gmail.com>
2024-05-21 10:44:36 -0300

    Criando testes que retornam lista vazia, lista com dados e tipo da lista

--- Cod3rsGrowth.Testes/TesteCarro.cs
+++ Cod3rsGrowth.Testes/TesteCarro.cs
@@ -1,13 +1,14 @@
 ﻿using Xunit;
-using Cod3rsGrowth.Dominio.Enums;
 using Cod3rsGrowth.Dominio.Entities;
 using Cod3rsGrowth.Dominio.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Cod3rsGrowth.Testes.ConfiguracaoAmbienteTeste;
 using Cod3rsGrowth.Dominio.Services;
+using Cod3rsGrowth.Dominio.Enums;
 
 namespace Cod3rsGrowth.Testes
 {
+
     public class TesteCarro : TesteBase
     {
         private readonly IServicoCarro _servicoCarro;
@@ -16,7 +17,6 @@ namespace Cod3rsGrowth.Testes
         {
             _servicoCarro = ServiceProvider.GetService<IServicoCarro>()
                 ?? throw new Exception($"Erro ao obter servico [{nameof(IServicoCarro)}]");
-
         }
 
         [Fact]
@@ -30,6 +30,41 @@ namespace Cod3rsGrowth.Testes
             //asset
             Assert.NotNull(carros);
             Assert.IsType<List<Carro>>(carros);
+        }
+
+        [Fact]
+        public void ObterTodos_deve_retornar_lista_vazia()
+        {
+            //arrange
+
+            //act
+            var carros = _servicoCarro.ObterTodos();
+
+            //asset
+            Assert.NotNull(carros);
+            Assert.Equal(0, carros?.Count);
+        }
+
+        [Fact]
+        public void ObterTodos_deve_retornar_lista_com_dados()
+        {
+            //arrange
+
+            //act
+            var carros = _servicoCarro.ObterTodos();
+            var novocarro = new Carro
+            {
+                Modelo = "Civic",
+                Marca = Marcas.Honda,
+                ValorDoVeiculo = 100
+            };
+            carros.Add(novocarro);
+
+            //asset
+            Assert.NotNull(carros);
+            Assert.Equivalent(novocarro.Modelo, carros[0].Modelo);
+            Assert.Equivalent(novocarro.Marca, carros[0].Marca);
+            Assert.Equivalent(novocarro.ValorDoVeiculo, carros[0].ValorDoVeiculo);
         }
     }
 }
--- Cod3rsGrowth.Testes/TesteVenda.cs
+++ Cod3rsGrowth.Testes/TesteVenda.cs
@@ -31,5 +31,39 @@ namespace Cod3rsGrowth.Testes
             Assert.IsType<List<Venda>>(vendas);
         }
 
+        [Fact]
+        public void ObterTodos_deve_retornar_lista_vazia()
+        {
+            //arrange
+
+            //act
+            var vendas = _servicoVenda.ObterTodos();
+
+            //asset
+            Assert.NotNull(vendas);
+            Assert.Equal(0, vendas?.Count);
+        }
+
+        [Fact]
+        public void ObterTodos_deve_retornar_lista_com_dados()
+        {
+            //arrange
+
+            //act
+            var vendas = _servicoVenda.ObterTodos();
+            var novavenda = new Venda
+            {
+                Nome = "Higor",
+                DataDeCompra = DateTime.Now,
+                ValorTotal = 100
+            };
+            vendas.Add(novavenda);
+
+            //asset
+            Assert.NotNull(vendas);
+            Assert.Equivalent(novavenda.Nome, vendas[0].Nome);
+            Assert.Equivalent(novavenda.DataDeCompra, vendas[0].DataDeCompra);
+            Assert.Equivalent(novavenda.ValorTotal, vendas[0].ValorTotal);
+        }
     }
 }
