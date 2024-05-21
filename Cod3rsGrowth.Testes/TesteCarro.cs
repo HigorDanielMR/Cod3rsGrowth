bf6c6172ed2d7a1d8054fe8207279d8fdd836dbb
Higor <higordaniel199@gmail.com>
2024-05-21 09:31:18 -0300

    Separando testes para TesteCarro e TesteVenda

---
+++ Cod3rsGrowth.Testes/TesteCarro.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+using Cod3rsGrowth.Dominio.Enums;
+using Cod3rsGrowth.Dominio.Entities;
+using Cod3rsGrowth.Dominio.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Cod3rsGrowth.Testes.ConfiguracaoAmbienteTeste;
+using Cod3rsGrowth.Dominio.Services;
+
+namespace Cod3rsGrowth.Testes
+{
+    public class TesteCarro : TesteBase
+    {
+        private readonly IServicoCarro _servicoCarro;
+
+        public TesteCarro()
+        {
+            _servicoCarro = ServiceProvider.GetService<IServicoCarro>()
+                ?? throw new Exception($"Erro ao obter servico [{nameof(IServicoCarro)}]");
+
+        }
+
+        [Fact]
+        public void ObterTodos_deve_retornar_lista_vazia()
+        {
+            //arrange
+
+            //act
+            var carros = _servicoCarro.ObterTodos();
+
+            //asset
+            Assert.NotNull(carros);
+            Assert.Equal(0, carros?.Count);
+        }
+
+        [Fact]
+        public void Criar_deve_salvar_Carro_dentro_do_banco()
+        {
+            //arrange
+            var carro = new Carro()
+            {
+                Cor = Cores.Azul,
+                Marca = Marcas.Honda,
+                Modelo = "asdf",
+                ValorDoVeiculo = 12000
+            };
+            _servicoCarro.Criar(carro);
+
+            //act
+            var carros = _servicoCarro.ObterTodos();
+            //asset
+            Assert.NotNull(carros);
+            Assert.Equal(1, carros?.Count); 
+        }
+    }
+}
---
+++ Cod3rsGrowth.Testes/TesteVenda.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+using Cod3rsGrowth.Dominio.Entities;
+using Cod3rsGrowth.Dominio.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Cod3rsGrowth.Testes.ConfiguracaoAmbienteTeste;
+
+namespace Cod3rsGrowth.Testes
+{
+
+    public class TesteVenda : TesteBase
+    {
+        private readonly IServicoVenda _servicoVenda;
+
+        public TesteVenda()
+        {
+            _servicoVenda = ServiceProvider.GetService<IServicoVenda>()
+                ?? throw new Exception($"Erro ao obter servico [{nameof(IServicoVenda)}]");
+        }
+
+        [Fact]
+        public void ObterTodos_deve_retornar_lista_vazia()
+        {
+            //arrange
+
+            //act
+            var vendas = _servicoVenda.ObterTodos();
+
+            //asset
+            Assert.NotNull(vendas);
+            Assert.Equal(0, vendas?.Count);
+        }
+
+        [Fact]
+        public void Criar_deve_salvar_Venda_dentro_do_banco()
+        {
+            var venda = new Venda()
+            {
+                Nome = "Higor",
+                Cpf = "0808916812868971",
+                Email = "jhlkajshlhasl@",
+                Telefone = "09029097102012",
+                DataDeCompra = DateTime.Now,
+                ValorTotal = 12000
+            };
+            _servicoVenda.Criar(venda);
+
+            //act
+            var vendas = _servicoVenda.ObterTodos();
+
+            //asset
+            Assert.NotNull(vendas);
+            Assert.Equal(1, vendas?.Count);
+        }
+    }
+}
