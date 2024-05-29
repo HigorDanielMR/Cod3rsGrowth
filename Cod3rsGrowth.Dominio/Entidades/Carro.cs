62cd9ba25ddf951dd060ef31da33290e43a98ef0
Higor <higordaniel199@gmail.com>
2024-05-29 09:45:54 -0300

    Refatorando codigo

--- Cod3rsGrowth.Dominio/Entidades/Carro.cs
+++ Cod3rsGrowth.Dominio/Entidades/Carro.cs
@@ -6,7 +6,7 @@ namespace Cod3rsGrowth.Dominio.Entities
     {
         public int Id { get; set; }
         public Marcas Marca { get; set; }
-        public string? Modelo { get; set; }
+        public string Modelo { get; set; }
         public Cores Cor { get; set; }
         public decimal ValorDoVeiculo { get; set; }
         public bool Flex { get; set; }
--- Cod3rsGrowth.Dominio/Entidades/Venda.cs
+++ Cod3rsGrowth.Dominio/Entidades/Venda.cs
@@ -3,11 +3,11 @@
     public class Venda
     {
         public int Id { get; set; }
-        public string? Nome { get; set; }
-        public string? Cpf { get; set; }
-        public string? Email { get; set; }
-        public string? Telefone { get; set; }
-        public List<int>? ItensVendidos { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public List<int> ItensVendidos { get; set; }
         public DateTime DataDeCompra { get; set; }
         public decimal ValorTotal { get; set; }
         public bool Pago { get; set; }
