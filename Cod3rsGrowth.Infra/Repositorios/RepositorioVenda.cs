abfb3e93b59d2bc4fb68023b4ff5746de838e428
Higor <higordaniel199@gmail.com>
2024-06-06 12:06:31 -0300

    Fazendo cnexão com banco de dados, criando arquivo app.config, criando interface de repositorio no dominio e criando repositorios no infra

---
+++ Cod3rsGrowth.Infra/Repositorios/RepositorioVenda.cs
@@ -0,0 +1,31 @@
+﻿using Cod3rsGrowth.Dominio.Entities;
+using Cod3rsGrowth.Dominio.Interfaces;
+
+namespace Cod3rsGrowth.Infra.Repositorios
+{
+    class RepositorioVenda : IRepositorio<Venda>
+    {
+        public List<Venda> ObterTodos()
+        {
+            return ObterTodos();
+        }
+
+        public Venda ObterPorId(int IdDeBusca)
+        {
+            return ObterPorId(IdDeBusca);
+        }
+
+        public Venda Criar(Venda venda)
+        {
+            return venda;
+        }
+
+        public Venda Editar(Venda vendaAtualizada)
+        {
+            return vendaAtualizada;
+        }
+        public void Remover(int Id)
+        {
+        }
+    }
+}
