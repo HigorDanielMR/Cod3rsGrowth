91995249ac91d5d1b6fa2e295874c06dbd90745f
Higor <higordaniel199@gmail.com>
2024-06-11 13:12:31 -0300

    Removendo entidade HistorioDeVendas

--- Cod3rsGrowth.Dominio/Entidades/HistoricoDeVendas.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cod3rsGrowth.Dominio.Entidades
-{
-    public class HistoricoDeVendas
-    {
-        public int Id { get; set; }
-        public int IdVenda { get; set; }
-        public int IdCarro { get; set; }
-    }
-}
