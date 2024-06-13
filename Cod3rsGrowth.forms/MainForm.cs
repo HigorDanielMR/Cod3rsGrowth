870cb22e393ac0241e343b74ea17e8f32e07c606
Higor <higordaniel199@gmail.com>
2024-06-13 09:19:55 -0300

    Criando injeção de dependencia no forms

---
+++ Cod3rsGrowth.forms/MainForm.cs
@@ -0,0 +1,21 @@
+namespace Cod3rsGrowth.forms
+{
+    public partial class MainForm : Form
+    {
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
