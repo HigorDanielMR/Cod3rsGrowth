8aaf9b3d1c7fbac5be39cb082603b35b5cb147de
Higor <higordaniel199@gmail.com>
2024-06-13 10:21:47 -0300

    Refatorando codigo

--- Cod3rsGrowth.forms/FormListagem.cs
+++ Cod3rsGrowth.forms/FormListagem.cs
@@ -2,18 +2,12 @@ namespace Cod3rsGrowth.forms
 {
     public partial class FormListagem : Form
     {
-
         public FormListagem()
         {
             InitializeComponent();
         }
 
         private void MainForm_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
         {
 
         }
