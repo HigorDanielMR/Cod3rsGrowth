1dc93ff619ab7b38692dd5190b3e1d157425a57b
Higor <higordaniel199@gmail.com>
2024-06-21 11:47:54 -0300

    Refatorando codigo

---
+++ Cod3rsGrowth.Testes/Servicos/ServicoCarro.cs
@@ -0,0 +1,56 @@
+﻿using FluentValidation;
+using Cod3rsGrowth.Dominio.Entidades;
+using Cod3rsGrowth.Dominio.Interfaces;
+using Cod3rsGrowth.Servicos.Validadores;
+
+namespace Cod3rsGrowth.Servicos.Servicos
+{
+    public class ServicoCarro
+    {
+        private ValidacoesCarro _validadorCarro;
+        private readonly IRepositorioCarro _repositorioCarro;
+
+        public ServicoCarro(IRepositorioCarro repositorioCarro, ValidacoesCarro validadorCarro)
+        {
+            _validadorCarro = validadorCarro;
+            _repositorioCarro = repositorioCarro;
+        }
+
+        public List<Carro> ObterTodos(FiltroCarro carro)
+        {
+            return _repositorioCarro.ObterTodos(carro);
+        }
+
+        public Carro ObterPorId(int IdDoItem)
+        {
+            return _repositorioCarro.ObterPorId(IdDoItem);
+        }
+
+        public Carro Criar(Carro carro)
+        {
+            var resultado = _validadorCarro.Validate(carro);
+            if (!resultado.IsValid)
+            {
+                var erros = string.Join(Environment.NewLine, resultado.Errors.Select(x => x.ErrorMessage));
+                throw new ValidationException(erros);
+            }
+            return _repositorioCarro.Criar(carro);
+        }
+
+        public Carro Editar(Carro carro)
+        {
+            var resultado = _validadorCarro.Validate(carro);
+            if (!resultado.IsValid)
+            {
+                var erros = string.Join(Environment.NewLine, resultado.Errors.Select(x => x.ErrorMessage));
+                throw new ValidationException(erros);
+            }
+            return _repositorioCarro.Editar(carro);
+        }
+
+        public void Remover(int IdDeRemocao)
+        {
+            _repositorioCarro.Remover(IdDeRemocao);
+        }
+    }
+}
---
+++ Cod3rsGrowth.Testes/Servicos/ServicoVenda.cs
@@ -0,0 +1,56 @@
+﻿using FluentValidation;
+using Cod3rsGrowth.Dominio.Entidades;
+using Cod3rsGrowth.Dominio.Interfaces;
+using Cod3rsGrowth.Servicos.Validadores;
+
+namespace Cod3rsGrowth.Servicos.Servicos
+{
+    public class ServicoVenda
+    {
+        private ValidacoesVenda _validadorVenda;
+        private readonly IRepositorioVenda _repositorioVenda;
+
+        public ServicoVenda(IRepositorioVenda repositorioVenda, ValidacoesVenda validacaoVenda)
+        {
+            _validadorVenda = validacaoVenda;
+            _repositorioVenda = repositorioVenda;
+        }
+
+        public List<Venda> ObterTodos(FiltroVenda venda)
+        {
+            return _repositorioVenda.ObterTodos(venda);
+        }
+
+        public Venda ObterPorId(int IdDoItem)
+        {
+            return _repositorioVenda.ObterPorId(IdDoItem);
+        }
+
+        public Venda Criar(Venda venda)
+        {
+            var resultado = _validadorVenda.Validate(venda);
+            if (!resultado.IsValid)
+            {
+                var erros = string.Join(Environment.NewLine, resultado.Errors.Select(x => x.ErrorMessage));
+                throw new ValidationException(erros);
+            }
+            return _repositorioVenda.Criar(venda);
+        }
+
+        public Venda Editar(Venda venda)
+        {
+            var resultado = _validadorVenda.Validate(venda, options => options.IncludeRuleSets("Editar").IncludeRulesNotInRuleSet());
+            if (!resultado.IsValid)
+            {
+                var erros = string.Join(Environment.NewLine, resultado.Errors.Select(x => x.ErrorMessage));
+                throw new ValidationException(erros);
+            }
+            return _repositorioVenda.Editar(venda);
+        }
+
+        public void Remover(int IdDeRemocao)
+        {
+            _repositorioVenda.Remover(IdDeRemocao);
+        }
+    }
+}
