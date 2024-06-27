7d5d4cbba17447f8271e0d94bd29b999bf90a924
Higor <higordaniel199@gmail.com>
2024-06-27 14:03:55 -0300

    Começando a configurar api web

--- Cod3rsGrowth.Web/Program.cs
+++ Cod3rsGrowth.Web/Program.cs
@@ -1,15 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,5 +18,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("Carro", () => "Civic, Honda, R$ 100.000,00");
 
 app.Run();
