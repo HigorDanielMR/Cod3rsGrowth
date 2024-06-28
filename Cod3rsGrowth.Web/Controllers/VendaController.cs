12fdbe5bd95a2bedfd5a2527aa6b523d0dea0f63
Higor <higordaniel199@gmail.com>
2024-06-28 11:13:23 -0300

    Criando controllers para carro e venda, criando crud

--- Cod3rsGrowth.Web/Controllers/VendaController.cs
+++ Cod3rsGrowth.Web/Controllers/VendaController.cs
@@ -1,4 +1,4 @@
-using Cod3rsGrowth.Dominio.Entidades;
+﻿using Cod3rsGrowth.Dominio.Entidades;
 using Cod3rsGrowth.Servicos.Servicos;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,43 +6,43 @@ namespace Cod3rsGrowth.Web.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CarroController : ControllerBase
+    public class VendaController : ControllerBase
     {
-        private readonly ServicoCarro _servico;
-        private FiltroCarro _filtro = new();
-        public CarroController(ServicoCarro servico)
+        private readonly ServicoVenda _servico;
+        private FiltroVenda _filtro = new();
+        public VendaController(ServicoVenda servico)
         {
             _servico = servico;
         }
 
-        [HttpGet("Carros")]
-        public IActionResult ObterTodosCarros()
+        [HttpGet("Vendas")]
+        public IActionResult ObterTodasAsVendas()
         {
             try
             {
-                var carros = _servico.ObterTodos(_filtro);
+                var vendas = _servico.ObterTodos(_filtro);
 
-                return Ok(carros);
+                return Ok(vendas);
             }
-            catch (FluentValidation.ValidationException ex)
+            catch(FluentValidation.ValidationException ex)
             {
                 return BadRequest(ex.Message);
             }
         }
 
-        [HttpGet ("Carros/{Id}")]
-        public IActionResult ObterCarroPorId(int Id)
+        [HttpGet("Vendas/{Id}")]
+        public IActionResult ObterVendaPorId(int Id)
         {
             try
             {
-                var carro = _servico.ObterPorId(Id);
+                var venda = _servico.ObterPorId(Id);
 
-                if (carro == null)
+                if (venda == null)
                 {
                     return NotFound();
                 }
 
-                return Ok(carro);
+                return Ok(venda);
             }
             catch(FluentValidation.ValidationException ex)
             {
@@ -50,18 +50,18 @@ namespace Cod3rsGrowth.Web.Controllers
             }
         }
 
-        [HttpPost("Carros")]
-        public IActionResult CriarCarro([FromBody]Carro carro)
+        [HttpPost("Vendas")]
+        public IActionResult CriarVenda([FromBody] Venda venda)
         {
             try
             {
-                if (carro == null)
+                if (venda == null)
                 {
                     return BadRequest();
                 }
 
-                _servico.Criar(carro);
-                return CreatedAtRoute(new { }, carro);
+                _servico.Criar(venda);
+                return CreatedAtRoute(new { }, venda);
             }
             catch(FluentValidation.ValidationException ex)
             {
@@ -69,17 +69,17 @@ namespace Cod3rsGrowth.Web.Controllers
             }
         }
 
-        [HttpPut("Carros/{Id}")]
-        public IActionResult EditarCarro(int Id, [FromBody]Carro carro)
+        [HttpPut("Vendas/{Id}")]
+        public IActionResult EditarVenda(int Id, [FromBody] Venda venda)
         {
             try
             {
-                if(carro == null ||  carro.Id != Id)
+                if (venda == null || venda.Id != Id)
                 {
                     return BadRequest();
                 }
 
-                _servico.Editar(carro);
+                _servico.Editar(venda);
                 return NoContent();
             }
             catch(FluentValidation.ValidationException ex)
@@ -88,13 +88,13 @@ namespace Cod3rsGrowth.Web.Controllers
             }
         }
 
-        [HttpDelete("Carros/{Id}")]
-        public IActionResult RemoverCarro(int Id)
+        [HttpDelete("Vendas/{Id}")]
+        public IActionResult RemoverVenda(int Id)
         {
             try
             {
-                var carro = _servico.ObterPorId(Id);
-                if(carro == null)
+                var venda = _servico.ObterPorId(Id);
+                if (venda == null)
                 {
                     return NotFound();
                 }
--- Cod3rsGrowth.Web/Program.cs
+++ Cod3rsGrowth.Web/Program.cs
@@ -1,6 +1,27 @@
+using Cod3rsGrowth.Dominio.Interfaces;
+using Cod3rsGrowth.Infra.ConexaoComBanco;
+using Cod3rsGrowth.Infra.Repositorios;
+using Cod3rsGrowth.Servicos.Servicos;
+using Cod3rsGrowth.Servicos.Validadores;
+using System.Configuration;
+using static LinqToDB.Common.Configuration;
+using ConfigurationManager = System.Configuration.ConfigurationManager;
+
+
+
+var stringDeConexao = ConfigurationManager.ConnectionStrings["ConexaoComBanco"].ToString();
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddScoped(provider => new MeuContextoDeDados(stringDeConexao));
+builder.Services.AddScoped<ServicoCarro>();
+builder.Services.AddScoped<ServicoVenda>();
+builder.Services.AddScoped<ValidacoesVenda>();
+builder.Services.AddScoped<ValidacoesCarro>();
+builder.Services.AddScoped<IRepositorioVenda, RepositorioVenda>();
+builder.Services.AddScoped<IRepositorioCarro, RepositorioCarro>();
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,6 +40,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapGet("Carro", () => "Civic, Honda, R$ 100.000,00");
+app.UseDeveloperExceptionPage();
 
 app.Run();
