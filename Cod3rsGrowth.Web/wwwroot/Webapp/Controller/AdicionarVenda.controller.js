0e81e4da721ff8fe2870465e81b50968f352abbe
Higor <higordaniel199@gmail.com>
2024-07-23 22:14:29 -0300

    feat: Criando tabela de seleção para selecionar carro, criando formatadores para cor e marca

--- Cod3rsGrowth.Web/wwwroot/Webapp/Controller/AdicionarVenda.controller.js
+++ Cod3rsGrowth.Web/wwwroot/Webapp/Controller/AdicionarVenda.controller.js
@@ -1,21 +1,44 @@
 ﻿sap.ui.define([
     "ui5/carro/controller/BaseController",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/core/routing/History",
+    "sap/ui/model/json/JSONModel",
+    "ui5/carro/model/formatter"
 
-], function (BaseController, JSONModel) {
+], function (BaseController, History, JSONModel, Formatter) {
     "use strict";
 
     var url = "http://localhost:5071/api/Carros"    
     var NomeDaAPI = "Carros"
 
     return BaseController.extend("ui5.carro.controller.AdicionarVenda", {
+        formatter: Formatter,
+
         onInit() {
             fetch(url)
                 .then((res) => res.json())
                 .then((data) => {
-                    this.getView().setModel(new JSONModel(data), NomeDaAPI);
+                    const jsonModel = new JSONModel(data)
+
+                    this.getView().setModel(jsonModel, NomeDaAPI);
                 })
                 .catch((err) => console.error(err));
+        },
+
+        handleLoadItems: function (oControlEvent) {
+            oControlEvent.getSource().getBinding("items").resume();
+        },
+
+        cancelar() {
+            var history, previousHash;
+
+            history = History.getInstance();
+            previousHash = history.getPreviousHash();
+
+            if (previousHash !== undefined) {
+                window.history.go(voltarUmaPagina);
+            } else {
+                this.getRouter().navTo("appListagem", {}, true /*no history*/);
+            }
         }
     });
 }, true);
