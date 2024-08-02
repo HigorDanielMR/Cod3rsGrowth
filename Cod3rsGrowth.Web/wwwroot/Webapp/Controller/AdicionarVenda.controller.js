ffb982cfebf0db213dfeb31145503d7c0b11c580
Higor <higordaniel199@gmail.com>
2024-08-02 14:07:43 -0300

    feat: Criando logica para carregar dados para inputs, carregar carros do metodo editar e comecando a organizar fetch para metodo patch

--- Cod3rsGrowth.Web/wwwroot/Webapp/Controller/AdicionarVenda.controller.js
+++ Cod3rsGrowth.Web/wwwroot/Webapp/Controller/AdicionarVenda.controller.js
@@ -9,6 +9,8 @@
 ], function (BaseController, History, JSONModel, Formatter, validacao, MessageBox) {
     "use strict";
 
+    let idVenda;
+    var _Rota;
     const estiverVazio = 0;
     const primeiroCarro = 0;
     const voltarUmaPagina = -1;
@@ -21,9 +23,9 @@
     const RotaCriar = "appAdicionarVenda"
     const idDoInputTelefone = "InputTelefone"
     const idDaTabelaCarrosDisponiveis = "TabelaCarrosDisponiveis"
-    let urlCarro = "http://localhost:5071/api/Carros/Disponiveis"
+    let urlCarrosDisponiveis = "http://localhost:5071/api/Carros/Disponiveis"
     let urlObterVendaPorId = "http://localhost:5071/api/Vendas/"
-
+    const urlCarroObterPorId = "http://localhost:5071/api/Carros/"
 
     return BaseController.extend("ui5.carro.controller.AdicionarVenda", {
         formatter: Formatter,
@@ -33,7 +35,7 @@
         },
 
         _carregarCarros() {
-            fetch(urlCarro)
+            fetch(urlCarrosDisponiveis)
                 .then((res) => res.json())
                 .then((data) => {
                     const jsonModel = new JSONModel(data)
@@ -46,10 +48,17 @@
         aoCoincidirRota() {
             this.processarEvento(() => {
                 var rota = sap.ui.core.UIComponent.getRouterFor(this);
-                rota.getRoute("appEditarVenda").attachMatched(this._mudarTituloDaView, this);
-                rota.getRoute("appEditarVenda").attachMatched(this._obterVendaPorId, this);
+                rota.getRoute("appEditarVenda").attachPatternMatched(this._carregarEventosEditar, this);
+
                 rota.getRoute(RotaCriar).attachMatched(this._carregarCarros, this);
             });
+        },
+
+        _carregarEventosEditar(oEvent) {
+            this._mudarTituloDaView();
+            this._obterVendaPorId(oEvent);
+            var oRouter = this.getRouter();
+            _Rota = oRouter.getRoute(oEvent.getParameter("name"))._oConfig.name;
         },
 
         aoColetarNome() {
@@ -87,32 +96,58 @@
 
         aoClicarNoBotaoAdicionarDeveAdicionarVenda() {
             this.processarEvento(() => {
-                const urlPost = "http://localhost:5071/api/Vendas/";
+                const url = "http://localhost:5071/api/Vendas/";
                 const carroEscolhido = this._obterCarroSelecionado();
                 const data = Date.now();
 
-                const venda = {
-                    "nome": this.aoColetarNome(),
-                    "cpf": this.aoColetarCpf(),
-                    "email": this.aoColetarEmail(),
-                    "telefone": this.aoColetarTelefone(),
-                    "idDoCarroVendido": carroEscolhido.id,
-                    "dataDeCompra": this.formatarData(data),
-                    "valorTotal": carroEscolhido.valorDoVeiculo,
-                    "pago": this.aoObterStatusPagamento()
+                if (_Rota === "appEditarVenda") {
+                    const urlEditar = url + idVenda;
+                    const venda = {
+                        "id": idVenda,
+                        "nome": this.aoColetarNome(),
+                        "cpf": this.aoColetarCpf(),
+                        "email": this.aoColetarEmail(),
+                        "telefone": this.aoColetarTelefone(),
+                        "idDoCarroVendido": carroEscolhido.id,
+                        "valorTotal": carroEscolhido.valorDoVeiculo,
+                        "pago": this.aoObterStatusPagamento()
+                    }
+
+                    const metodo = "PATCH"
+                    this._requisicaoHTTP(metodo, urlEditar, venda)
                 }
+                else if (_Rota === RotaCriar) {
+                    const venda = {
+                        "nome": this.aoColetarNome(),
+                        "cpf": this.aoColetarCpf(),
+                        "email": this.aoColetarEmail(),
+                        "telefone": this.aoColetarTelefone(),
+                        "idDoCarroVendido": carroEscolhido.id,
+                        "dataDeCompra": this.formatarData(data),
+                        "valorTotal": carroEscolhido.valorDoVeiculo,
+                        "pago": this.aoObterStatusPagamento()
+                    }
+                    const metodo = "POST"
+                    this._requisicaoHTTP(metodo, url, venda)
+                    this.getView().byId("sucessoAoCriarVenda").setVisible(true);
 
-                fetch(urlPost, {
-                    method: "POST",
-                    body: JSON.stringify(venda),
-                    headers: { "Content-type": "application/json; charset=UTF-8" }
-                })
-                    .then(response => response.json())
-                    .then(json => console.log(json))
-                    .catch(err => console.log(err));
+                }
+                else {
+                    this.getView().byId("erroAoCriarVenda").setVisible(true);
 
-                this.getView().byId("sucessoAoCriarVenda").setVisible(true);
+                }
             })
+        },
+
+        _requisicaoHTTP(metodo, url, venda) {
+            fetch(url, {
+                method: metodo,
+                body: JSON.stringify(venda),
+                headers: { "Content-type": "application/json; charset=UTF-8" }
+            })
+                .then(response => response.json())
+                .then(json => console.log(json))
+                .catch(err => console.log(err));
         },
 
         _obterCarroSelecionado() {
@@ -139,14 +174,50 @@
             return Titulo;
         },   
 
-        _obterVendaPorId: function (oEvent) {
-            let id = oEvent.getParameter("arguments").id;
-            let query = urlObterVendaPorId + id;
+        _obterVendaPorId(oEvent) {
+            idVenda = oEvent.getParameter("arguments").id;
+            let query = urlObterVendaPorId + idVenda;
             fetch(query)
                 .then(resp => resp.json())
-                .then(venda => this.getView().setModel(new JSONModel(venda), modeloVenda));
+                .then(venda => {
 
-            
+                    const id = this._carregarDados(venda)
+                    this._carregarCarrosEditar(id)
+                });
+        },
+
+        _carregarCarrosEditar(id) {
+
+            let query = urlCarroObterPorId + id;
+            let carroEspecifico;
+
+            fetch(urlCarrosDisponiveis)
+                .then(res => res.json())
+                .then(data => {
+                    this.getView().setModel(new JSONModel(data), modeloCarro);
+                    return fetch(query);
+                })
+                .then(resp => resp.json())
+                .then(carro => {
+                    carroEspecifico = carro;
+
+                    const listaDeCarros = this.getView().getModel(modeloCarro).getData();
+                    listaDeCarros.push(carroEspecifico);
+
+                    this.getView().getModel(modeloCarro).setData(listaDeCarros);
+                })
+                .catch(err => {
+                    MessageBox.error(err);
+                });
+        },
+
+        _carregarDados(venda) {
+            const nome = this.oView.byId(idDoInputNome).setValue(venda.nome);
+            const cpf = this.oView.byId(idDoInputCpf).setValue(venda.cpf);
+            const email = this.oView.byId(idDoInputEmail).setValue(venda.email);
+            const telefone = this.oView.byId(idDoInputTelefone).setValue(venda.telefone);
+
+            return venda.idDoCarroVendido;
         },
 
         aoClicarDeveVoltarParaATelaDeListagem() {
